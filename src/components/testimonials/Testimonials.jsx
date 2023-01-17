d790c5df986771cc42dd794c6677e7c6185723e2
Nick Fortis <fortisn7@gmail.com>
2023-01-17 13:27:48 -0500

    Added experience and services sections

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './testimonials.css'
 
+/* When I have Work Experience, Fill This Section */
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Testimonials</h5>
+      <h2>Work I Have Done</h2>
+    </section>
   )
 }
 
